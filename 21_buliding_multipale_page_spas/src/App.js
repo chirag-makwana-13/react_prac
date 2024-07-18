03166c56b78bff1880fe5123d97e6fed723a115f
Chirag Makwana <chirag13m@gmail.com>
2024-07-18 17:10:28 +0530

    21 folder videos watched and implemention

---
+++ 21_buliding_multipale_page_spas/src/App.js
@@ -0,0 +1,32 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import ErrorPage from './pages/Error';
+import HomePage from './pages/Home';
+import ProductDetailPage from './pages/ProductDetail';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage/>}   />
+//   </Route>
+// )
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<RootLayout/>,
+    errorElement:<ErrorPage/>,
+    children:[
+      { path: '/', element: <HomePage /> },
+      { path:'/products' , element:<ProductsPage/>},
+      { path: '/products/:productId', element: <ProductDetailPage /> }
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
